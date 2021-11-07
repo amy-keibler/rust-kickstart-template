2af481973df07faccd8816ff2abfd41b020e7dea
Amy Keibler <amelia.keibler@gmail.com>
2021-11-07 17:52:01 -0500

    Initial Rust Kickstart Template

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rust Starter Kickstart Template
+
+This is a [kickstart](https://github.com/Keats/kickstart) template for building a Rust application.
+
+## Variables
+
+### `project_name`
+
+The name of the project in lowercase snake case form. This is used in the Cargo.toml file and flake file.
+
+### `project_description`
+
+The short description of the purpose of the project. This is used in the Cargo.toml file and the flake file.
+
+### `license`
+
+The license of the project. Currently supports the [Be Gay, Do Crime](https://twitter.com/scanlime/status/1304825753029107712) license and a dual-license of [MIT](https://mit-license.org/) and/or [Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0.html).
---
+++ {{ project_name }}/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "{{ project_description }}";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.{{ project_name }} = naersk-lib.buildPackage {
+        pname = "{{ project_name }}";
+        root = ./.;
+      };
+      defaultPackage = packages.{{ project_name }};
+
+      # `nix run`
+      apps.{{ project_name }} = utils.lib.mkApp {
+        drv = packages.{{ project_name }};
+        exePath = "/bin/{{ project_name }}";
+      };
+      defaultApp = apps.{{ project_name }};
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo clippy rustfmt rust-analyzer ];
+
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+}
