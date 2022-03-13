00c1a199aa37a12172869da57f969331ee1509e3
Amy Keibler <amelia.keibler@gmail.com>
2022-03-12 21:59:03 -0500

    Add application type
    - Add support for an empty Rust project and a CLI project
    - Clean up some formatting and whitespace

---
+++ {{ project_name }}/src/cli.rs
@@ -0,0 +1,6 @@
+use clap::Parser;
+
+/// {{ project_description }}
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {}
--- {{ project_name }}/src/main.rs
+++ {{ project_name }}/src/main.rs
@@ -1,3 +1,21 @@
+{%- if application_type == "empty" -%}
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Amy!");
 }
+{%- elif application_type == "cli" -%}
+use clap::Parser;
+
+mod cli;
+
+use cli::Args;
+
+fn main() -> eyre::Result<()> {
+    let _args = Args::parse();
+
+    tracing_subscriber::fmt::init();
+    color_eyre::install()?;
+    println!("Hello, Amy!");
+
+    Ok(())
+}
+{%- endif %}
